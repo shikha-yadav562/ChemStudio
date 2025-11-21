678cd12877ae40e5d7f774613cde17d516cd6cdb
shikha-yadav562 <ysikha642@gmail.com>
2025-11-21 17:44:32 +0530

    Save my local changes

--- lib/DB/database_helper.dart
+++ lib/DB/database_helper.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+
 
 class DatabaseHelper {
   static const _dbName = 'chemstudio.db';
