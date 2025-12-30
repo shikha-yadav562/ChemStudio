6d2251f1ef6d3a4b044ec659420aaa16f55d2598
shikha-yadav562 <ysikha642@gmail.com>
2025-12-30 23:50:15 +0530

    A group 1 anlaysis done

--- lib/screens/WET_TEST/A_WET/group0/group0analysis.dart
+++ lib/screens/WET_TEST/A_WET/group0/group0analysis.dart
@@ -156,7 +156,7 @@ void _next() async {  // ✅ Add async
               const LinearGradient(colors: [accentTeal, primaryBlue])
                   .createShader(bounds),
           child: const Text(
-            'Salt C : Wet Test',
+            'Salt A : Wet Test',
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
